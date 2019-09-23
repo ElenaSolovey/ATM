3a9ea541b724727e7aef439cda25d52058031ee8
Helen_Solovey <soloveyhelen0@gmail.com>
2019-09-23 14:26:55 +0300

    add testFactory

--- ATMTask/src/aura/WithdrawMoneyComponent/WithdrawMoneyComponentController.js
+++ ATMTask/src/aura/WithdrawMoneyComponent/WithdrawMoneyComponentController.js
@@ -13,23 +13,27 @@
         let commission = component.get("v.atm.Bank__r.Commission__c");
         console.log(commission);
         let amountToWithdraw = component.get("v.card.Amount_to_withdraw__c");
-        console.log(amountToWithdraw);
-        let atmBank = component.get("v.atm.Bank__c");
-        console.log(atmBank);
-        let cardBank = component.get("v.card.Bank__c");
-        console.log(cardBank);
-        let finalSum;
-        if( atmBank === cardBank){
-            finalSum = parseFloat(amountToWithdraw);
-            console.log(finalSum);
-        } else {
-            finalSum = (parseFloat(amountToWithdraw) + (parseFloat(amountToWithdraw) * (commission*0.01)));
-            console.log(finalSum);
-        }
-        if(moneyATM <= finalSum){
-            alert("Sorry, but there is not enough money in the ATM");
-        } else {
-            helper.withdrawMoney(component, finalSum);
+        if(amountToWithdraw < 0){
+
+        }else {
+            console.log(amountToWithdraw);
+            let atmBank = component.get("v.atm.Bank__c");
+            console.log(atmBank);
+            let cardBank = component.get("v.card.Bank__c");
+            console.log(cardBank);
+            let finalSum;
+            if (atmBank === cardBank) {
+                finalSum = parseFloat(amountToWithdraw);
+                console.log(finalSum);
+            } else {
+                finalSum = (parseFloat(amountToWithdraw) + (parseFloat(amountToWithdraw) * (commission * 0.01)));
+                console.log(finalSum);
+            }
+            if (moneyATM <= finalSum) {
+                alert("Sorry, but there is not enough money in the ATM");
+            } else {
+                helper.withdrawMoney(component, finalSum);
+            }
         }
     }
 });
